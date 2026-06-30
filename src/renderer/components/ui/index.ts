806d49d8ed93ebc75f311ccf2f2f0682f04cfd73
itxskbaloch4-jpg <itxskbaloch4@gmail.com>
2026-06-30 04:40:28 +0200

    Create index.ts

---
+++ src/renderer/components/ui/index.ts
@@ -0,0 +1,5 @@
+// Minimal UI primitives – no shadcn dependency needed
+export { Button } from './Button'
+export { Input } from './Input'
+export { Modal } from './Modal'
+export { Spinner } from './Spinner'
